25e0e36b1556b5ebfb8b677ef764b94743eb58cd
Stuart Padley <stuartpa@hotmail.com>
2025-05-01 06:33:13 -0700

    Add MIT License to per file header

--- MssqlMcp/MssqlMcp.Tests/UnitTests.cs
+++ MssqlMcp/MssqlMcp.Tests/UnitTests.cs
@@ -1,4 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
 
 using Microsoft.Extensions.Logging;
 using Moq;
--- MssqlMcp/MssqlMcp/Program.cs
+++ MssqlMcp/MssqlMcp/Program.cs
@@ -1,4 +1,5 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
 
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
