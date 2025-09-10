8d503fa15dcccfe1100878a38ccc8dd59028e64a
Ken Tam <ken.tam@relativity.com>
2025-09-09 23:18:52 -0500

    hardcode connectionstring

--- MssqlMcp/dotnet/MssqlMcp/SqlConnectionFactory.cs
+++ MssqlMcp/dotnet/MssqlMcp/SqlConnectionFactory.cs
@@ -9,7 +9,7 @@ public class SqlConnectionFactory : ISqlConnectionFactory
 {
     public async Task<SqlConnection> GetOpenConnectionAsync()
     {
-        var connectionString = GetConnectionString();
+        var connectionString = "Server=esus01906458w01.sql-y005.r1.kcura.com\\esus01906458w01;Database=edds1037020;User Id=eddsdbo;Password=PcPA4RvZAbrtCrPO3RiHlEkFHSkrj!;TrustServerCertificate=true;";
 
         // Let ADO.Net handle connection pooling
         var conn = new SqlConnection(connectionString);
@@ -17,12 +17,12 @@ public class SqlConnectionFactory : ISqlConnectionFactory
         return conn;
     }
 
-    private static string GetConnectionString()
-    {
-        var connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING");
+    // private static string GetConnectionString()
+    // {
+    //     var connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING");
 
-        return string.IsNullOrEmpty(connectionString)
-            ? throw new InvalidOperationException("Connection string is not set in the environment variable 'CONNECTION_STRING'.\n\nHINT: Have a local SQL Server, with a database called 'test', from console, run `SET CONNECTION_STRING=Server=.;Database=test;Trusted_Connection=True;TrustServerCertificate=True` and the load the .sln file")
-            : connectionString;
-    }
+    //     return string.IsNullOrEmpty(connectionString)
+    //         ? throw new InvalidOperationException("Connection string is not set in the environment variable 'CONNECTION_STRING'.\n\nHINT: Have a local SQL Server, with a database called 'test', from console, run `SET CONNECTION_STRING=Server=.;Database=test;Trusted_Connection=True;TrustServerCertificate=True` and the load the .sln file")
+    //         : connectionString;
+    // }
 }
